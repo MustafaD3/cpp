b5eb2a036e453922f2d6bb9355a76b814ff07870
Mustafa Dalkılıç <81879117+MustafaD3@users.noreply.github.com>
2026-02-10 08:50:33 +0300

    Add files via upload

--- cpp_module_01/ex01/src/Zombie.cpp
+++ cpp_module_01/ex01/src/Zombie.cpp
@@ -1,21 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mdalkili <mdalkilic344@student.42.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/19 22:06:01 by mdalkili          #+#    #+#             */
-/*   Updated: 2025/09/19 22:06:01 by mdalkili         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Zombie.hpp"
 Zombie::Zombie()
 {
 }
 Zombie::~Zombie()
 {
+	std::cout << this->_name << ": Destroyed" << std::endl;
 }
 void Zombie::announce( void )
 {
